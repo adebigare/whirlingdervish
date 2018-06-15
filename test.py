a3bb30eaf8c8e0bb13674ac733d4ba7abce8918a
adebigare <linearsynergy@gmail.com>
2018-06-15 19:32:25 -0400

    Pads change the lights!

--- test.py
+++ test.py
@@ -2,15 +2,32 @@
 
 #also written by dlaw. see license in lights.py
 
-import lights, colorsys, time
+import colorsys, time
+from lights import PowerSupply
+from evdev import InputDevice, categorize, ecodes
 
-pds = lights.PowerSupply('192.168.0.101')
+FRAMERATE = 30.0
+PAD_PATH = "/dev/input/by-id/usb-mayflash_limited_MAYFLASH_GameCube_Controller_Adapter-event-joystick"
 
+pad = InputDevice(PAD_PATH)
+pds = PowerSupply('192.168.0.101')
 hue = 0
-val = 0
-while True:
-    val = (val + .01) % 1
-    color = colorsys.hsv_to_rgb(hue, 1, val)
+
+print(pad)
+
+def update_hue(hue):
+    color = colorsys.hsv_to_rgb(hue, 1, 1)
     pds.set_all(color)
     pds.update()
-    time.sleep(0.05)
+
+
+for event in pad.read_loop():
+    if event.type == ecodes.EV_KEY and event.value == 1:
+        if event.code == 300: #up
+            update_hue(0.0)
+        if event.code == 301: #right
+            update_hue(0.2)
+        if event.code == 302: #down
+            update_hue(0.4)
+        if event.code == 303: #left
+            update_hue(0.6)
