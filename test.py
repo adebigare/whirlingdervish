93643a0116979e8efd99272fc0e2d91f6a49abd2
adebigare <linearsynergy@gmail.com>
2018-06-15 19:55:05 -0400

    Threading and pulse on pad steps

--- test.py
+++ test.py
@@ -2,32 +2,54 @@
 
 #also written by dlaw. see license in lights.py
 
-import colorsys, time
+import colorsys
+from time import sleep
 from lights import PowerSupply
 from evdev import InputDevice, categorize, ecodes
+from select import select
+from threading import Thread
+from Queue import Queue
 
 FRAMERATE = 30.0
 PAD_PATH = "/dev/input/by-id/usb-mayflash_limited_MAYFLASH_GameCube_Controller_Adapter-event-joystick"
 
 pad = InputDevice(PAD_PATH)
 pds = PowerSupply('192.168.0.101')
-hue = 0
+events = Queue()
 
-print(pad)
-
-def update_hue(hue):
-    color = colorsys.hsv_to_rgb(hue, 1, 1)
-    pds.set_all(color)
-    pds.update()
-
-
-for event in pad.read_loop():
+def handle_event(event):
     if event.type == ecodes.EV_KEY and event.value == 1:
         if event.code == 300: #up
-            update_hue(0.0)
+            events.put(0.0)
         if event.code == 301: #right
-            update_hue(0.2)
+            events.put(0.2)
         if event.code == 302: #down
-            update_hue(0.4)
+            events.put(0.4)
         if event.code == 303: #left
-            update_hue(0.6)
+            events.put(0.6)
+
+
+def input_loop():
+    for event in pad.read_loop():
+        handle_event(event)
+
+input_thread = Thread(target=input_loop)
+input_thread.daemon = True
+input_thread.start()
+
+hue = 0
+val = 0
+
+while True:
+    if not events.empty():
+        hue = events.get_nowait()
+        val = 1
+    color = colorsys.hsv_to_rgb(hue, 1, val)
+    pds.set_all(color)
+    pds.update()
+    val = val * 0.85
+    if val <= 0.05:
+        val = 0.0
+    sleep(1.0/FRAMERATE)
+
+
