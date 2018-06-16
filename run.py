22b7be4b91576541256c7d5be1baaa25bfde10d8
adebigare <linearsynergy@gmail.com>
2018-06-15 21:09:14 -0400

    Multiple chasers

--- run.py
+++ run.py
@@ -9,46 +9,74 @@ from evdev import InputDevice, categorize, ecodes
 from threading import Thread
 from Queue import Queue
 
+# Constants
+
 FRAMERATE = 30.0
 PAD_PATH = "/dev/input/by-id/usb-mayflash_limited_MAYFLASH_GameCube_Controller_Adapter-event-joystick"
 
-pad = InputDevice(PAD_PATH)
-pds = PowerSupply('192.168.0.101')
-events = Queue()
+# Animations
+
+class Chaser(object):
+    def __init__(self, hue, speed):
+        self.done = False
+        self._hue = hue
+        self._speed = speed
+        self._fidx = 0.0
+
+    def update(self, pixels):
+        for i in range(3):
+            idx = self._idx - i
+            if idx < 0:
+                continue
+            val = 1.0 - (i * 0.4)
+            color = colorsys.hsv_to_rgb(self._hue, 1, val)
+            pixels[idx] = color
+        self._fidx += self._speed
+        if self._idx >= 50:
+            self.done = True 
+
+    @property
+    def _idx(self):
+        return int(self._fidx)
+
+# Input
+
+inputs = Queue()
 
 def handle_event(event):
     if event.type == ecodes.EV_KEY and event.value == 1:
         if event.code == 300: #up
-            events.put(0.0)
+            inputs.put((0.0, 0.6))
         if event.code == 301: #right
-            events.put(0.2)
+            inputs.put((0.2, 0.3))
         if event.code == 302: #down
-            events.put(0.4)
+            inputs.put((0.4, 1.0))
         if event.code == 303: #left
-            events.put(0.6)
-
+            inputs.put((0.6, 1.5))
 
 def input_loop():
+    pad = InputDevice(PAD_PATH)
     for event in pad.read_loop():
         handle_event(event)
 
-input_thread = Thread(target=input_loop)
-input_thread.daemon = True
-input_thread.start()
+def main():
+    pds = PowerSupply('192.168.0.101')
+    input_thread = Thread(target=input_loop)
+    input_thread.daemon = True
+    input_thread.start()
 
-hue = 0
-val = 0
+    chasers = [] 
 
-while True:
-    if not events.empty():
-        hue = events.get_nowait()
-        val = 1
-    color = colorsys.hsv_to_rgb(hue, 1, val)
-    pds.set_all(color)
-    pds.update()
-    val = val * 0.85
-    if val <= 0.05:
-        val = 0.0
-    sleep(1.0/FRAMERATE)
+    while True:
+        if not inputs.empty():
+            (hue, speed) = inputs.get_nowait() 
+            chasers.append(Chaser(hue, speed))
+        pds.clear()
+        for chaser in chasers:
+            chaser.update(pds.rgb)
+        chasers = [chaser for chaser in chasers if not chaser.done]
+        pds.update()
+        sleep(1.0/FRAMERATE)
 
-
+if __name__== "__main__":
+      main()
